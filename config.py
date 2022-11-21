e0757c89df3f08961f0166311ce9e97fc090c007
mackenziejj <60846695+mackenziejj@users.noreply.github.com>
2022-11-22 09:55:31 +1100

    Updating crednetials

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 [default]
-aws_access_key_id = AKIAYVP4CIPPMZXBWWOP
-aws_secret_access_key = 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
+aws_access_key_id = AKIAYVP4CIPPB7QTJ3MD
+aws_secret_access_key = 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
 output = json
+region = us-east-2
