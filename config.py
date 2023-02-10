7c07cbdb3e9daac0b502913e2f9171879005c11c
Mackenzie <mackenziejackson@live.com>
2023-02-10 11:54:50 +0100

    credentials

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
-[default]
-aws_access_key_id = AKIAYVP4CIPPKIU25JOE
-aws_secret_access_key = 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
-output = json
-region = us-east-2
+
+    "access_key_id": "AKIAVMEV7Q32J73V7WO2",
+    "access_key_secret": "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",
+    "name": "honeyalert1",
+    "tags": {
+      "source": "vcs",
+      "tag_1": "GitHub",
+      "tag_2": "Backend-Project",
+      "tag_3": "Secret-Repo"
