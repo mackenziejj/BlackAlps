1d72913ee7b356ecd57bad23d2856529ecd30e37
Mackenzie <mackenziejackson@live.com>
2023-02-14 11:15:54 +0100

    testing

--- config.py
+++ config.py
@@ -1,9 +1,6 @@
 
+
+
     "access_key_id": "AKIAVMEV7Q32J73V7WO2",
     "access_key_secret": "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",
     "name": "honeyalert1",
-    "tags": {
-      "source": "vcs",
-      "tag_1": "GitHub",
-      "tag_2": "Backend-Project",
-      "tag_3": "Secret-Repo"
